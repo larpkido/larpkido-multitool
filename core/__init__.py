3d147f252c61a3ad556a964bab77051fb62bd8a4
larpkido <stanislo.minecraft@gmail.com>
2026-06-26 17:07:40 +0200

    Add files via upload

---
+++ core/__init__.py
@@ -0,0 +1,11 @@
+#   _                     _    _     _       
+#  | |    __ _ _ __ _ __ | | _(_) __| | ___  
+#  | |   / _` | '__| '_ \| |/ / |/ _` |/ _ \ 
+#  | |__| (_| | |  | |_) |   <| | (_| | (_) |
+#  |_____\__,_|_|  | .__/|_|\_\_|\__,_|\___/ 
+#                  |_|                       
+# 
+# Larpkido Multitool - Developed by Larpkido
+# Discord: https://discord.gg/larpkido
+
+# core package
